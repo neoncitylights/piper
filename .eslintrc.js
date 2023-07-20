164abea7da01a5adcf0ba06abd86a5fa79c93398
Samantha <contact@samanthanguyen.me>
2023-07-19 21:17:20 -0500

    chore: make ECMAScript version consistent between TS and ESLint
    Bumps from ES2017 to ES2022.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'env': {
 		'browser': true,
-		'es2021': true,
+		'es2022': true,
 		'node': true,
 	},
 	'extends': [
@@ -12,7 +12,7 @@ module.exports = {
 	],
 	'parser': '@typescript-eslint/parser',
 	'parserOptions': {
-		'ecmaVersion': 2017,
+		'ecmaVersion': 2022,
 		'sourceType': 'module',
 	},
 	'plugins': [
