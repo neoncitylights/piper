6c21396db1b985e28ba30de56fd061e5fafe4308
Samantha Nguyen <contact@samanthanguyen.me>
2023-12-15 23:31:24 -0600

    chore: rename library to @neoncitylights/piper

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'src/index.ts'),
-			name: 'html-walker',
-			fileName: (format) => `html-walker.${format}.js`,
+			name: 'piper',
+			fileName: (format) => `piper.${format}.js`,
 		},
 	},
 	plugins: [
